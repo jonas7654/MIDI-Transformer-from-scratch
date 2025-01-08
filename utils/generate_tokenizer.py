c27980c2635f2d72a1c86a7a67dcc983532f92f9
Jonas V <me322@uni-heidelberg.de>
2025-01-08 14:31:36 +0100

    Added the midi files. Added tok.pdf where I keep notes of what i did.
    MAIN CHANGES:
    -Added tokenizer generation
    -Added train, val and test set generation

---
+++ utils/generate_tokenizer.py
@@ -0,0 +1,50 @@
+from random import shuffle
+import os
+import numpy as np
+from icecream import ic
+
+from miditok import REMI, TokenizerConfig  # here we choose to use REMI
+from miditok.data_augmentation import augment_dataset
+from miditok.utils import split_files_for_training
+from pathlib import Path
+
+# Specify the directories and configs
+current_dir = os.getcwd()
+data_dir = os.path.join(os.path.join("..", current_dir), "datasets")
+files_path = list(Path(data_dir).glob("**/*.mid"))
+
+# Specify the vocab length
+"""
+NOTE: Eventhough we specified vocab_size it does not mean that the final size is equal to our value.
+      If the value is to low in order to create a "meaningful" vocabulary or when it is to large the finale choice will differ
+      See tokenizer.vocab_size
+"""
+vocab_size = 4096
+
+
+# Create the Tokenizer
+# :TODO : This is just random right now
+TOKENIZER_PARAMS = {
+    "pitch_range": (21, 109),
+    "beat_res": {(0, 4): 8, (4, 12): 4},
+    "num_velocities": 32,
+    "special_tokens": ["PAD", "BOS", "EOS", "MASK"],
+    "use_chords": True,
+    "use_rests": False,
+    "use_tempos": True,
+    "use_time_signatures": False,
+    "use_programs": False,
+    "num_tempos": 32,  # number of tempo bins
+    "tempo_range": (40, 250),  # (min, max)
+}
+config = TokenizerConfig(**TOKENIZER_PARAMS)
+
+# Creates the tokenizer
+tokenizer = REMI(config)
+
+# Train the tokenizer
+tokenizer.train(vocab_size = vocab_size, files_paths = files_path)
+ic(tokenizer.vocab_size)
+# Save the tokenizer
+tokenizer.save(Path(current_dir,"tokenizer.json"))
+
