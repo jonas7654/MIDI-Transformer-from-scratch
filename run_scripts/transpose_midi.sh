91b52d77a37bd05fad9e89e3878dac2fca1d3acc
hpdc04 <you@example.com>
2025-01-13 13:18:27 +0100

    @author : Jonas
    Working on memory optimization

---
+++ run_scripts/transpose_midi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+
+# load appropriate conda paths, because we are not in a login shell
+eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+conda activate eml
+
+current_dir=($pwd)
+
+echo "Pre-processing raw midi files"
+cd ../CUPY/models/utils/
+python3 transpy_jonas_version.py
+cd $current_dir
