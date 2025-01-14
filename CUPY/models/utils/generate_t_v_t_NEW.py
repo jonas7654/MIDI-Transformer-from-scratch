ea36d6386f990368b59964807156b82a449ba91c
hpdc04 <you@example.com>
2025-01-14 14:18:05 +0100

    @Jonas: Worked on generating train, valid and test sets in parallel to speed up computing

--- CUPY/models/utils/generate_t_v_t_NEW.py
+++ CUPY/models/utils/generate_t_v_t_NEW.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 from icecream import ic
 from pathlib import Path
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
 
 from miditok import REMI, TokenizerConfig  # here we choose to use REMI
@@ -28,7 +28,7 @@ current_dir = os.getcwd()
 data_path = os.path.join(os.path.dirname(current_dir) , "datasets")
 name_of_midi_data = "transposed_midi"
 
-files_path = list(Path(data_path, name_of_midi_data).glob("*.mid")) 
+midi_paths = list(Path(data_path, name_of_midi_data).glob("*.mid")) 
 tokenizer_path = os.path.join(os.path.dirname(current_dir), "tokenizers/")
 
 seq_length = 128
@@ -38,9 +38,10 @@ tokenizer = REMI(params = Path(tokenizer_path, "tokenizer.json"))
 ic(tokenizer.vocab_size)
 
 # Split the dataset into train/valid/test subsets, with 15% of the data for each of the two latter
-midi_paths = files_path
 
 total_num_files = len(midi_paths)
+print(f"Total number of midi files: {total_num_files}")
+
 num_files_valid = round(total_num_files * 0.15)
 num_files_test = round(total_num_files * 0.15)
 shuffle(midi_paths)
@@ -48,34 +49,35 @@ midi_paths_valid = midi_paths[:num_files_valid]
 midi_paths_test = midi_paths[num_files_valid:num_files_valid + num_files_test]
 midi_paths_train = midi_paths[num_files_valid + num_files_test:]
 
-# Chunk MIDIs and perform data augmentation on each subset independently
-for files_paths, subset_name in (
-    (midi_paths_train, "train"), (midi_paths_valid, "val"), (midi_paths_test, "test")
-):
+
+# # Chunk MIDIs and perform data augmentation on each subset independently
+# for files_paths, subset_name in (
+#     (midi_paths_train, "train"), (midi_paths_valid, "val"), (midi_paths_test, "test")
+# ):
         
-    # Split the MIDIs into chunks of sizes approximately about 'seq_length' tokens
+#     # Split the MIDIs into chunks of sizes approximately about 'seq_length' tokens
     
-    subset_chunks_dir = Path(f"{data_path}/dataset_{subset_name}")
-    if not os.path.exists(subset_chunks_dir):
-        print(f"Creating directory: /dataset_{subset_name}")
-        os.makedirs(subset_chunks_dir)
+#     subset_chunks_dir = Path(f"{data_path}/dataset_{subset_name}")
+#     if not os.path.exists(subset_chunks_dir):
+#         print(f"Creating directory: /dataset_{subset_name}")
+#         os.makedirs(subset_chunks_dir)
         
 
-    #split_files_for_training(
-    #    files_paths=files_paths,
-    #    tokenizer=tokenizer,
-    #    save_dir=subset_chunks_dir,
-    #    max_seq_len=seq_length,
-    #    num_overlap_bars=2,
-    #)
+#     #split_files_for_training(
+#     #    files_paths=files_paths,
+#     #    tokenizer=tokenizer,
+#     #    save_dir=subset_chunks_dir,
+#     #    max_seq_len=seq_length,
+#     #    num_overlap_bars=2,
+#     #)
 
-    # Perform data augmentation
-#    augment_dataset(
-#        subset_chunks_dir,
-#        pitch_offsets=[-12, 12],
-#        velocity_offsets=[-4, 4],
-#        duration_offsets=[-0.5, 0.5],
-#    )    
+#     # Perform data augmentation
+# #    augment_dataset(
+# #        subset_chunks_dir,
+# #        pitch_offsets=[-12, 12],
+# #        velocity_offsets=[-4, 4],
+# #        duration_offsets=[-0.5, 0.5],
+# #    )    
 
     
 """
@@ -97,18 +99,26 @@ def collator(input, seq_length):
 
 def process_midi_file(midi_file, seq_length, tokenizer, collator):
     midi_file_tokenized = tokenizer(Path(midi_file))[0].ids
-    return collator(midi_file_tokenized)
+    print(f"processed {midi_file}")
+    return collator(midi_file_tokenized, seq_length)
     
 # Create train, val, test token datasets
 
+midi_path_dict = {
+    "train" : midi_paths_train,
+    "valid" : midi_paths_valid,
+    "test" : midi_paths_valid
+}
 
-for subset_name in ("train", "valid", "test"):
-    files_path = f"midi_paths_{subset_name}"
-    print(f"test: {files_path[0]}")
-    dataset_tokenized = np.zeros((len(files_paths), seq_length))
+for subset in midi_path_dict:
+    number_of_subset_files = len(midi_path_dict[subset])
+    print(f"Generating {subset} binary data with length: {number_of_subset_files}")
+
+    files_path = midi_path_dict[subset]
+    dataset_tokenized = np.zeros((number_of_subset_files, seq_length))
     
     # Iterate over all midi files and tokenize them in parallel
-    with ThreadPoolExecutor() as executor:
+    with ProcessPoolExecutor(max_workers = 128) as executor:
         future_to_idx = {executor.submit(process_midi_file, midi_file, seq_length, tokenizer, collator): i for i, midi_file in enumerate(files_path)}
 
 
@@ -117,18 +127,16 @@ for subset_name in ("train", "valid", "test"):
             try:
                 dataset_tokenized[i, :] = future.result()
             except Exception as e:
-                print(f"Error with file : {files_path[i]}")
+                print(f"Error with file: {files_path[i]}, Error: {e}")
 
     # Sanity check
     assert np.all(dataset_tokenized < tokenizer.vocab_size), "Found out-of-vocabulary tokens in dataset"
     ic(dataset_tokenized[:100])
 
     # SAVE
-    save_dir = os.path.join(output_path, f"{subset_name}.bin")
+    save_dir = os.path.join(output_path, f"{subset}.bin")
     Path(output_path).mkdir(parents = True, exist_ok = True)
     dataset_tokenized.astype(np.uint16).tofile(save_dir)
 
 ic(tokenizer.vocab_size)
 
-
-print("WARNING: make sure that the dataset_ folders were empty before")
