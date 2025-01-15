ba0faabdb7f21a70a1db13c75c4e085a3da6a106
hpdc04 <you@example.com>
2025-01-15 13:01:47 +0100

    @Jonas: Working on faster data pre processing

--- run_scripts/parameters.sh
+++ run_scripts/parameters.sh
@@ -1,6 +1,6 @@
 export context_length=128
-export epochs=5
+export epochs=15
 export batch_size=16
 export eval_interval=1 # default : 100
-export lr=0.1
+export lr=0.2
 export log_interval=1 # default : 100
