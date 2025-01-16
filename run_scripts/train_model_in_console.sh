804e0e1d95dd9646b38bc42ce30c2e5b09508120
hpdc04 <you@example.com>
2025-01-16 09:54:28 +0100

    @Jonas implemented new training routine

--- run_scripts/train_model_in_console.sh
+++ run_scripts/train_model_in_console.sh
@@ -1,29 +1,13 @@
-#!/bin/bash
-#SBATCH --gres gpu:2
-#SBATCH --mem 5G
-#SBATCH --cpus-per-task 1
-#SBATCH --time 500:00
-#SBATCH -p exercise-eml
-#SBATCH -o logs/model_py_GPU.log
-
-
-# load appropriate conda paths, because we are not in a login shell
-eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
-conda activate eml
-
-# load model parameters
 source /csghome/hpdc04/Transformer_Code/run_scripts/parameters.sh
-
-echo "Running Training (with CUPY)"
 
 echo "context length: $context_length"
 echo "epochs: $epochs"
 echo "batch size: $batch_size"
 echo "eval_interval: $eval_interval"
 echo "learning rate: $lr"
+echo "log interval: $log_interval"
 
-
-python ../CUPY/models/GoePT/model.py \
+srun --mem=5G --cpus-per-task=1 --gres gpu:2 --partition=exercise-eml --pty python ../CUPY/models/GoePT/train.py \
     --data-dir /csghome/hpdc04/Transformer_Code/CUPY/models/datasets/tokenized \
     --vocab-file /csghome/hpdc04/Transformer_Code/CUPY/models/tokenizers/tokenizer.json \
     --checkpoint-dir /csghome/hpdc04/Transformer_Code/checkpoints \
@@ -31,5 +15,6 @@ python ../CUPY/models/GoePT/model.py \
     --epochs "$epochs" \
     --batch-size "$batch_size" \
     --eval-interval "$eval_interval" \
-    --lr "$lr"
-                                     
+    --lr "$lr" \
+    --log-interval "$log_interval"
+
