e4a7d7336da087770ba519e0fce8b60906c44164
hpdc04 <you@example.com>
2025-01-16 11:28:41 +0100

    @Jonas: small bugfix with ADAM

--- run_scripts/parameters.sh
+++ run_scripts/parameters.sh
@@ -1,6 +1,6 @@
 export context_length=128
 export epochs=50
 export batch_size=16
-export eval_interval=10 # default : 100
-export lr=0.1
-export log_interval=10 # default : 100
+export eval_interval=2 # default : 100
+export lr=0.2
+export log_interval=2 # default : 100
