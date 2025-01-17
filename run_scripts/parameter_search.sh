a7dbdf5ab5ca3ea21ddb90a199976d3a3495c182
hpdc04 <you@example.com>
2025-01-17 08:43:04 +0100

    @Jonas: started to implement parameter search routine via wandb

---
+++ run_scripts/parameter_search.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --gres gpu:2
+#SBATCH --mem 5G
+#SBATCH --cpus-per-task 1
+#SBATCH --time 500:00
+#SBATCH -p exercise-eml
+#SBATCH -o logs/parameter_search.log
+
+
+# load appropriate conda paths, because we are not in a login shell
+eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+conda activate eml
+
+echo "Running Parameter Search (with wandb sweeps)"
+
+
+python ../CUPY/models/GoePT/train_wandb_sweeps.py
+                                     
