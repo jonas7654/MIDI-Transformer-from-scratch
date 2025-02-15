033c65cfd497e605a2db143478c56b2e53e92edf
hpdc04 <you@example.com>
2025-02-15 12:29:33 +0100

    updated gitignore

--- run_scripts/inference.sh
+++ run_scripts/inference.sh
@@ -2,8 +2,8 @@ p=$1
 temperature=$2
 
 srun --mem=12G --cpus-per-task=1 --gres gpu:1 --partition=exercise-eml --pty python ../CUPY/models/GoePT/Inference.py \
-    --weights "/csghome/hpdc04/Transformer_Code/checkpoints/dedicated-quiver-27_3200.json" \
-    --vocab-file "/csghome/hpdc04/Transformer_Code/CUPY/models/tokenizers/tokenizer_REMI_4096_FULL_True.json" \
+    --weights "/csghome/hpdc04/Transformer_Code/checkpoints/resolute-chocolate-355_1775.json" \
+    --vocab-file "/csghome/hpdc04/Transformer_Code/CUPY/models/tokenizers/tokenizer_REMI_4096_FULL_False.json" \
     --input "/csghome/hpdc04/Transformer_Code/Inference/input_files/" \
     --b 512\
     --save-dir "/csghome/hpdc04/Transformer_Code/Inference/output_files/" \
