04342d34f4ddf595c86e45127c5eb5d7a1369670
Jonas Georg Veit <me322@csg-headnode.ziti.uni-heidelberg.de>
2026-05-08 18:42:00 +0200

    added an example inference run

--- run_scripts/inference.sh
+++ run_scripts/inference.sh
@@ -1,12 +1,11 @@
 p=$1
 temperature=$2
 
-srun --mem=12G --cpus-per-task=1 --gres gpu:1 --partition=exercise-eml --pty python ../CUPY/models/GoePT/Inference.py \
-    --weights "/csghome/hpdc04/Transformer_Code/checkpoints/loyal-chocolate-387_4125.json" \
-    --vocab-file "/csghome/hpdc04/Transformer_Code/CUPY/models/tokenizers/tokenizer_REMI_4096_FULL_False.json" \
-    --input "/csghome/hpdc04/Transformer_Code/Inference/input_files/" \
-    --b 256\
-    --save-dir "/csghome/hpdc04/Transformer_Code/Inference/output_files/" \
-    --manually-set-sos-eos-trunc true \
-    --p $p \
-    --temperature $temperature
+srun --mem=12G --cpus-per-task=1 --gres gpu:1 --partition=brook --pty python ../CUPY/models/GoePT/Inference.py \
+  --weights "../checkpoints_fine_tuning/fine_tuning_dedicated-quiver-27_650_650_0.json" \
+  --vocab-file "../CUPY/models/tokenizers/tokenizer_REMI_4096_FULL_False.json" \
+  --input "../Inference/input_files/" \
+  --b 256 --save-dir "../Inference/output_files/" \
+  --manually-set-sos-eos-trunc true \
+  --p $p \
+  --temperature $temperature
